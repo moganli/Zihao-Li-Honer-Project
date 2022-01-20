b213ad9311e472099d57149dd71af921fd9b9791
moganli <morganli1216@gmail.com>
2022-01-21 04:19:03 +0800

    The basic GAN is completed, and the results are visualized

--- GAN/trainGan.py
+++ GAN/trainGan.py
@@ -51,10 +51,10 @@ def celoss_zero(logits):
     return tf.reduce_mean(loss)
 
 
-def d_loss_fn(G, D, batch_z, batch_x, is_training):
-    gimage = G(batch_z, is_training)
-    d_fake_logits = D(gimage, is_training)
-    d_real_logits = D(batch_x, is_training)
+def d_loss_fn(G, D, batch_z, batch_x, training):
+    gimage = G(batch_z, training)
+    d_fake_logits = D(gimage, training)
+    d_real_logits = D(batch_x, training)
 
     d_loss_real = celoss_one(d_real_logits)
     d_loss_fake = celoss_zero(d_fake_logits)
@@ -63,9 +63,9 @@ def d_loss_fn(G, D, batch_z, batch_x, is_training):
     return loss
 
 
-def g_loss_fn(G, D, batch_z, is_training):
-    fake_image = G(batch_z, is_training)
-    d_fake_logits = D(fake_image, is_training)
+def g_loss_fn(G, D, batch_z, training):
+    fake_image = G(batch_z, training)
+    d_fake_logits = D(fake_image, training)
     loss = celoss_one(d_fake_logits)
     return loss
 
@@ -119,9 +119,15 @@ def main():
             g_loss = g_loss_fn(G, D, batch_z, is_training)
         grads = tape.gradient(g_loss, G.trainable_variables)
         Goptimizer.apply_gradients(zip(grads, G.trainable_variables))
-
+        #每100次 存储一次用来观察
         if epoch % 100 == 0:
             print(epoch,'D loss',d_loss,'G loss', g_loss)
+
+            z = tf.random.normal([120, z_dim])
+            fake_image = G(z, training=False)
+            img_path = os.path.join(r'C:\Users\86135\Desktop\Zihao-Li-Honer-Project\images', 'gan_%d.png' % epoch)
+            save_result(fake_image.numpy(), 10, img_path, color_mode='P')
+
 
 if __name__ == '__main__':
     main()
